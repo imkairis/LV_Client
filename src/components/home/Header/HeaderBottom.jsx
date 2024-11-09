d9f7c5b67367adf2419c86d0b355204bf9264f7d
imkairis <khanghonhi62@gmail.com>
2024-11-09 22:08:05 +0700

    Bo tong quat

--- src/components/home/Header/HeaderBottom.jsx
+++ src/components/home/Header/HeaderBottom.jsx
@@ -193,7 +193,7 @@ const HeaderBottom = () => {
                   >
                     <Link to="/info">
                       <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
-                        Infomation
+                        <Infomation></Infomation>
                       </li>
                     </Link>
                     <li
