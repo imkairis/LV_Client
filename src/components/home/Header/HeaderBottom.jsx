6f22d0520cae2ca291dac94a4f111e3771d8b4a6
imkairis <khanghonhi62@gmail.com>
2024-11-09 20:12:22 +0700

    Sua header

--- src/components/home/Header/HeaderBottom.jsx
+++ src/components/home/Header/HeaderBottom.jsx
@@ -38,9 +38,9 @@ const HeaderBottom = () => {
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
-      setIsLoggedIn(true); // Người dùng đã đăng nhập
+      setIsLoggedIn(true);
     } else {
-      setIsLoggedIn(false); // Người dùng chưa đăng nhập
+      setIsLoggedIn(false);
     }
   }, []);
 
@@ -213,9 +213,6 @@ const HeaderBottom = () => {
             <Link to="/cart">
               <div className="relative">
                 <FaShoppingCart />
-                <span className="absolute font-titleFont top-3 -right-2 text-xs w-4 h-4 flex items-center justify-center rounded-full bg-primeColor text-white">
-                  {products.length > 0 ? products.length : 0}
-                </span>
               </div>
             </Link>
           </div>
