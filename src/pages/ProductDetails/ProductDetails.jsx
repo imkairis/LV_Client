5e02bcd18dd62cab10762900f6b323c1582f48fe
imkairis <khanghonhi62@gmail.com>
2024-11-13 18:17:04 +0700

    Sua sp

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -7,6 +7,7 @@ import Image from "../../components/designLayouts/Image";
 import { formatDate } from "../../utils/utils";
 import ImgCD from "../../assets/images/BottomProduct.png";
 import { useAddToCart } from "../../hooks/useAddToCart";
+
 const ProductDetails = () => {
   const location = useLocation();
   const { _id } = useParams();
@@ -14,6 +15,7 @@ const ProductDetails = () => {
   const [productInfo, setProductInfo] = useState({});
   const [activeImg, setActiveImage] = useState("");
   const [amount, setAmount] = useState(1);
+  const [isCollapsed, setIsCollapsed] = useState(true); // Trạng thái thu gọn
   const { handleAddToCart } = useAddToCart();
 
   useEffect(() => {
@@ -35,11 +37,11 @@ const ProductDetails = () => {
     <div className="w-full mx-auto border-b-[1px] border-b-gray-300 bg-white">
       {" "}
       {/* Màu nền toàn phần */}
-      <div className="max-w-container mx-auto px-8 bg-gray-50 py-8 rounded-lg">
+      <div className="max-w-container mx-auto px-8 bg-white py-8 rounded-lg">
         {" "}
         {/* Khung nền chứa nội dung */}
         <Breadcrumbs title="" prevLocation={prevLocation} />
-        <div className="flex flex-col justify-between lg:flex-row gap-16  mt-5">
+        <div className="flex flex-col justify-between lg:flex-row gap-8 mt-5">
           {/* Hình ảnh sản phẩm */}
           <div className="flex flex-col gap-6 lg:w-2/4">
             <div className="p-4 bg-white shadow-lg rounded-lg  border-gray-300">
@@ -52,35 +54,24 @@ const ProductDetails = () => {
           </div>
 
           {/* Thông tin sản phẩm */}
-          <div className="flex flex-col gap-4 lg:w-2/4 items-start">
-            <div>
-              <span className="text-violet-600 font-semibold">
-                {productInfo?.type?.name}
-              </span>
-              <h1 className="text-3xl font-bold">{productInfo.name}</h1>
-            </div>
-            <p className="text-gray-700">{productInfo.description}</p>
-            <h6 className="text-2xl font-semibold">
-              {parseInt(productInfo.price).toLocaleString("vi-VN")} VND
-            </h6>
-            <p className="text-gray-700">
-              <span className="text-gray-700">Xuất xứ:</span>{" "}
-              {productInfo.origin}
-            </p>
-            <p className="text-gray-700">
-              <span className="text-gray-700">Khối lượng:</span>{" "}
-              {productInfo.weight} kg
-            </p>
-            <p className="text-gray-700">
-              <span className="text-gray-700">Đối tượng sử dụng:</span>{" "}
+          <div className="bg-white shadow-lg rounded-lg p-6 border-gray-300 w-full">
+            <span className="text-violet-600 font-semibold mt-4">
+              {productInfo?.type?.name}
+            </span>
+            <h1 className="text-3xl font-bold leading-relaxed mt-4">
+              {productInfo.name}
+            </h1>
+
+            <p className="font-medium text-lg mt-4">
+              <strong>Đối tượng sử dụng:</strong>{" "}
               {productInfo?.targetAudience?.name}
             </p>
-            <p className="text-gray-700">
-              <span className="text-gray-700">Nhóm tuổi sử dụng:</span>{" "}
-              {productInfo?.ageGroup?.name}
-            </p>
-            <div className="flex flex-row items-center gap-12">
-              <div className="flex flex-row items-center">
+            <h6 className="text-2xl font-semibold leading-relaxed mt-4">
+              {parseInt(productInfo.price).toLocaleString("vi-VN")} VND
+            </h6>
+
+            <div className="flex flex-row items-center gap-12 mt-6">
+              <div className="flex flex-row items-center gap-4">
                 <button
                   className="bg-gray-200 py-2 px-5 rounded-lg text-violet-800 text-3xl"
                   onClick={() => setAmount((prev) => (prev > 1 ? prev - 1 : 1))}
@@ -105,24 +96,59 @@ const ProductDetails = () => {
           </div>
         </div>
         {/* Chi tiết sản phẩm bổ sung */}
-        <div className="mt-10 space-y-4">
-          <h1 className="font-bold text-[26px] mt-6">Thông tin sản phẩm:</h1>
-          <p className="font-medium text-lg">
-            <span className="font-normal">Hướng dẫn sử dụng:</span>{" "}
-            {productInfo.userManual || "Không có hướng dẫn sử dụng"}
-          </p>
-          <p className="font-medium text-lg">
-            <span className="font-normal">Thành phần:</span>{" "}
-            {productInfo.element}
-          </p>
-          <p className="font-medium text-lg">
-            <span className="font-normal">Ngày sản xuất:</span>{" "}
-            {formatDate(productInfo.dateOfManufacture)}
-          </p>
-          <p className="font-medium text-lg">
-            <span className="font-normal">Hạn sử dụng:</span>{" "}
-            {formatDate(productInfo.expirationDate)}
-          </p>
+        <div className="mt-16 mb-16 space-y-4">
+          <div className="bg-white shadow-lg rounded-lg p-6 border-gray-300">
+            <h1 className="font-bold text-[26px] mt-6 text-center mb-5">
+              Thông tin sản phẩm
+            </h1>
+
+            {/* Mô tả sản phẩm (luôn hiển thị) */}
+            <p className="font-medium text-lg" mt-4>
+              <strong>Mô tả sản phẩm:</strong> {productInfo.description}
+            </p>
+
+            {/* Hướng dẫn sử dụng (luôn hiển thị) */}
+            <p className="font-medium text-lg mt-4">
+              <strong>Hướng dẫn sử dụng:</strong>{" "}
+              {productInfo.userManual || "Không có hướng dẫn sử dụng"}
+            </p>
+
+            {/* Các thông tin khác sẽ bị thu gọn */}
+            {!isCollapsed && (
+              <>
+                <p className="font-medium text-lg mt-4">
+                  <strong>Xuất xứ:</strong> {productInfo.origin}
+                </p>
+                <p className="font-medium text-lg mt-4">
+                  <strong>Khối lượng:</strong> {productInfo.weight} kg
+                </p>
+
+                <p className="font-medium text-lg mt-4">
+                  <strong>Nhóm tuổi sử dụng:</strong>{" "}
+                  {productInfo?.ageGroup?.name}
+                </p>
+                <p className="font-medium text-lg mt-4">
+                  <strong>Thành phần:</strong> {productInfo.element}
+                </p>
+                <p className="font-medium text-lg mt-4">
+                  <strong>Ngày sản xuất:</strong>{" "}
+                  {formatDate(productInfo.dateOfManufacture)}
+                </p>
+                <p className="font-medium text-lg mt-4">
+                  <strong>Hạn sử dụng:</strong>{" "}
+                  {formatDate(productInfo.expirationDate)}
+                </p>
+              </>
+            )}
+
+            {/* Nút thu gọn/mở rộng */}
+            <button
+              onClick={() => setIsCollapsed(!isCollapsed)}
+              className="bg-violet-800 text-white font-semibold py-2 px-6 rounded-lg mt-4 mx-auto block"
+            >
+              {isCollapsed ? "Xem thêm" : "Thu gọn"}
+            </button>
+          </div>
         </div>
         {/* Hình ảnh bổ sung */}
         <div className="mt-6">
